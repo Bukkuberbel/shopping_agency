172a32f712cbd0474799cf4229668a46aa77f676
Takeshi Yanagihara <momotarousu@gmail.com>
2018-11-04 15:50:38 +0900

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
-  
+
+
+  get 'sessions/new'
+
+  root 'page#index'
+
   resources 'users'
-  resources 'requests'
+
+  resources 'carts'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  post '/add_item' => 'carts#add_item'
+  post '/update_item' => 'carts#update_item'
+  delete '/delete_item' => 'carts#delete_item'
+
+  resources 'carts' do
+    resources 'products'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
